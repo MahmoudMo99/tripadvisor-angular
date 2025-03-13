eb4fb668135f506fd455e653ff86ad1e85e4154e
manar255 <manarkhaled2510@gmail.com>
2025-03-13 13:31:52 +0200

    add profile component and update routing; disable CLI analytics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 // import { NavBarHomePageComponent } from './components/home/nav-bar-home-page/nav-bar-home-page.component';
 import { HomePageComponent } from './components/home/home-page/home-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: 'Home', component: HomePageComponent },
-  { path: '', redirectTo: '/Home', pathMatch: 'full' }
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  {path:'profile',component:ProfileComponent}
 ];
