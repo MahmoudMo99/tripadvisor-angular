5f5a372d372d3840865a31fa6505345d1c33d848
manar255 <manarkhaled2510@gmail.com>
2025-04-06 21:26:17 +0200

    add profile components and implement navigation; update styles

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FeedComponent } from './components/feed/feed.component';
+import { CommonModule, NgClass } from '@angular/common';
+import { TripComponent } from "./components/trip/trip.component";
+import { PhotosComponent } from "./components/photos/photos.component";
+import { ReviewsComponent } from "./components/reviews/reviews.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [CommonModule,FeedComponent, NgClass, TripComponent, PhotosComponent, ReviewsComponent],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  // nav
+  navItems = [
+    {name:'Activity feed', Component: FeedComponent},
+    {name:'Trip', Component: TripComponent},
+    {name:'Photo', Component: PhotosComponent},
+    {name:'Reviews', Component: ReviewsComponent},
+    {name:'Booking', Component: ReviewsComponent},
+    {name:'Travel map', Component: FeedComponent}
+  ];
+  currentMainIndex = 0;
 
+  // 
+
+
+  constructor(){}
+
+
+  // nav
+  changeMain(index: any){
+   this.currentMainIndex = index;
+  }
 }
