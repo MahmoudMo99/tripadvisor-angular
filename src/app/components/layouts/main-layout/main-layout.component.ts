62ed4fd00481f4a6fea9edfa71cd492eb9bea55e
shaima-abdelbaset <shaimaaabdelbasset1@gmail.com>
2025-05-07 14:01:28 +0300

    modal-language

--- src/app/components/layouts/main-layout/main-layout.component.ts
+++ src/app/components/layouts/main-layout/main-layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../navbar/navbar/navbar.component";
 import { FooterComponent } from "../../footer/footer.component";
 import { RouterOutlet } from '@angular/router';
+import { ModalLanguageComponent } from "../../../shared/modal-language/modal-language.component";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavbarComponent, FooterComponent,RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet, ModalLanguageComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
---
+++ src/app/shared/language-navbar.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageNavbarService {
+  private showModalSource = new Subject<void>();
+  showModal$ = this.showModalSource.asObservable();
+
+  constructor() {}
+
+  triggerModal() {
+    this.showModalSource.next();
+  }
+}
