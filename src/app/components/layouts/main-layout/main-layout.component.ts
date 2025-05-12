e6cf7e367331166bfa2b3a143bec50d3db0298d8
Mahmoud Mo <mmahmoud092002@gmail.com>
2025-05-12 19:37:37 +0300

    add to top button

--- src/app/components/layouts/main-layout/main-layout.component.ts
+++ src/app/components/layouts/main-layout/main-layout.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from "../../navbar/navbar/navbar.component";
-import { FooterComponent } from "../../footer/footer.component";
+import { Component, HostListener } from '@angular/core';
+import { NavbarComponent } from '../../navbar/navbar/navbar.component';
+import { FooterComponent } from '../../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { ModalLanguageComponent } from "../../../shared/modal-language/modal-language.component";
+import { ModalLanguageComponent } from '../../../shared/modal-language/modal-language.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavbarComponent, FooterComponent, RouterOutlet, ModalLanguageComponent],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    RouterOutlet,
+    ModalLanguageComponent,
+  ],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.scss'
+  styleUrl: './main-layout.component.scss',
 })
 export class MainLayoutComponent {
+  showScrollTop = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollTop = window.pageYOffset > 300;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
