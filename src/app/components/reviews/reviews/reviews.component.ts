3bcfb5ff747f2eb9b5c730f506964fd7562bdf94
manar255 <manarkhaled2510@gmail.com>
2025-05-18 01:04:11 +0300

    feat(reviews): add router link to 'Write a review' button and import RouterLink in reviews component

--- src/app/components/reviews/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews/reviews.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ReviewRateComponent } from "../review-form/review-rate/review-rate.component";
 import { ReviewCardComponent } from './review-card/review-card.component';
 import { ReviewProgressbarComponent } from "./review-progressbar/review-progressbar.component";
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-reviews',
-  imports: [ReviewRateComponent, ReviewCardComponent, ReviewProgressbarComponent],
+  imports: [ReviewRateComponent, ReviewCardComponent, ReviewProgressbarComponent,RouterLink],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss'
 })
